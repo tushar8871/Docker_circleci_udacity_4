30e44bf81fb20f13bfdf22cb0a9ff736c356cb2a
tushar8871 <tusharkhachane6@gmail.com>
2021-10-26 15:52:28 +0530

    Modify files

--- demos/flask-sklearn-student-starter/run_docker.sh
+++ demos/flask-sklearn-student-starter/run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image
+docker build -t docker-flask .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+docker run -itd docker-flask -p 80:80 docker
